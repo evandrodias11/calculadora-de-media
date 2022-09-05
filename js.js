6f3361f0052da30cb5e47aecce428e71ae35c862
Evandro Dias <evandro10dias@gmail.com>
2022-09-05 19:48:51 -0300

    update js

--- js.js
+++ js.js
@@ -1,13 +1,13 @@
 var nome = "Evandro";
-var notaDoPrimeiroBimestre = 9.4566;
-var notaDoSegundoBimestre = 7.2345;
-var notaDoTerceiroBimestre = 4.65675;
-var notaDoQuartoBimestre = 2.2678;
+var notaPrimeiroBimestre = 9.4566;
+var notaSegundoBimestre = 7.2345;
+var notaTerceiroBimestre = 4.65675;
+var notaQuartoBimestre = 2.2678;
 var notaFinal =
-  (notaDoPrimeiroBimestre +
-    notaDoSegundoBimestre +
-    notaDoTerceiroBimestre +
-    notaDoQuartoBimestre) /
+  (notaPrimeiroBimestre +
+    notoSegundoBimestre +
+    notoTerceiroBimestre +
+    notoQuartoBimestre) /
   4;
 
 var notaFormatada = notaFinal.toFixed(1);
